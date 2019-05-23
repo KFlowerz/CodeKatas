58c10bb5d7364860d465bce8ca505e4e8bf8cde4
PKs <k.flores@live.com.mx>
2019-05-22 20:57:30 -0500

    Add project files.

---
+++ KatasCore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KatasCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
